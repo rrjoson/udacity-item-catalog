1d039df39aa5fad963ab28941a6c1e77a4132e11
rrjoson <rr_joson@yahoo.com>
2016-10-26 03:34:45 +0800

    feat: Add basic database setup

---
+++ database_setup.py
@@ -0,0 +1,37 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class User(Base):
+	__tablename__ = 'user'
+
+	id = Column(Integer, primary_key=True)
+	name = Column(String(250), nullable=False)
+	email = Column(String(250), nullable=False)
+	picture = Column(String(250))
+
+class Category(Base):
+	__tablename__ = 'category'
+
+	id = Column(Integer, primary_key = True)
+	name = Column(String(80), nullable = False)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	user = relationship(User)
+
+class CategoryItem(Base):
+	__tablename__ = 'category_item'
+
+	name = Column(String(80), nullable = False)
+	description = Column(String(250))
+	id = Column(Integer, primary_key = True)
+	category_id = Column(Integer, ForeignKey('category.id'))
+	category = relationship(Category)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	user = relationship(User)
+
+engine = create_engine('sqlite:///itemcatalog.db')
+Base.metadata.create_all(engine)
