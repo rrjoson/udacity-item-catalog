a781eb7ba2cf69ce403e0bc4c4f6a664ecc4b81c
rrjoson <rr_joson@yahoo.com>
2016-10-31 15:29:33 +0800

    feat: Add url for views in categories page

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Category, CategoryItem, User
