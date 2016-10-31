fb8650206383779474c9a03d3df42405f898e13e
rrjoson <rr_joson@yahoo.com>
2016-10-31 12:53:46 +0800

    chore: Add description of catalog items in fakeitems

--- fakeitems.py
+++ fakeitems.py
@@ -37,17 +37,17 @@ category1 = Category(name="strings", user_id=1)
 session.add(category1)
 session.commit()
 
-item1 = CategoryItem(name="violin", user_id=1, category=category1)
+item1 = CategoryItem(name="violin", user_id=1, description="Violin, family of stringed musical instruments having wooden bodies whose backs and fronts are slightly convex, the fronts pierced by two-shaped resonance holes. The instruments of the violin family have been the dominant bowed instruments because of their versatility, brilliance, and balance of tone, and their wide dynamic range. A variety of sounds may be produced, e.g., by different types of bowing or by plucking the string (see pizzicato). The violin has always been the most important member of the family, from the beginning being the principal orchestral instrument and holding an equivalent position in chamber music and as a solo instrument. The technique of the violin was developed much earlier than that of the viola or cello.", category=category1)
 
 session.add(item1)
 session.commit()
 
-item2 = CategoryItem(name="viola", user_id=1, category=category1)
+item2 = CategoryItem(name="viola", user_id=1,  description="The viola is the alto instrument of the violin family (violin, viola, cello). It is constructed using the same components as the violin, the only difference being the larger size. ... In other words, the viola is too small in proportion to its tuning and this is the reason for its distinctive timbre.", category=category1)
 
 session.add(item2)
 session.commit()
 
-item3 = CategoryItem(name="cello", user_id=1, category=category1)
+item3 = CategoryItem(name="cello", user_id=1, description="The cello is used as a solo musical instrument, as well as in chamber music ensembles, string orchestras, as a member of the string section of symphony orchestras, and some rock bands. It is the second-largest bowed string instrument in the modern symphony orchestra, the double bass being the largest.", category=category1)
 
 session.add(item3)
 session.commit()
@@ -58,17 +58,17 @@ category2 = Category(name="woodwinds", user_id=1)
 session.add(category1)
 session.commit()
 
-item1 = CategoryItem(name="flute", user_id=1, category=category2)
+item1 = CategoryItem(name="flute", user_id=1, description="A musical wind instrument consisting of a tube with a series of fingerholes or keys, in which the wind is directed against a sharp edge, either directly, as in the modern transverse flute, or through a flue, as in the recorder. an organ stop with wide flue pipes, having a flutelike tone.", category=category2)
 
 session.add(item1)
 session.commit()
 
-item2 = CategoryItem(name="piccolo", user_id=1, category=category2)
+item2 = CategoryItem(name="piccolo", user_id=1,  description="Piccolo, in full flauto piccolo, highest-pitched woodwind instrument of orchestras and military bands. It is a small transverse (horizontally played) flute of conical or cylindrical bore, fitted with Boehm-system keywork and pitched an octave higher than the ordinary concert flute.", category=category2)
 
 session.add(item2)
 session.commit()
 
-item3 = CategoryItem(name="oboe", user_id=1, category=category2)
+item3 = CategoryItem(name="oboe", user_id=1, description="The oboe is a woodwind instrument in the soprano register. The blowing end of the oboe's slim conical tube (head) turns into a small metal pipe to which two reeds are affixed.", category=category2)
 
 session.add(item3)
 session.commit()
@@ -79,17 +79,17 @@ category3 = Category(name="percussion", user_id=1)
 session.add(category1)
 session.commit()
 
-item1 = CategoryItem(name="marimba", user_id=1, category=category3)
+item1 = CategoryItem(name="marimba", user_id=1, description="The marimba is a percussion instrument consisting of a set of wooden bars struck with mallets to produce musical tones. Resonators suspended underneath the bars amplify their sound. ... This instrument is a type of idiophone, but with a more resonant and lower-pitched tessitura than the xylophone.", category=category3)
 
 session.add(item1)
 session.commit()
 
-item2 = CategoryItem(name="timpani", user_id=1, category=category3)
+item2 = CategoryItem(name="timpani", user_id=1, description="Timpani or kettledrums are musical instruments in the percussion family. A type of drum, they consist of a skin called a head stretched over a large bowl traditionally made of copper. They are played by striking the head with a specialized drum stick called a timpani stick or timpani mallet. Timpani evolved from military drums to become a staple of the classical orchestra by the last third of the 18th century. Today, they are used in many types of musical ensembles, including concert bands, marching bands, orchestras, and even in some rock.", category=category3)
 
 session.add(item2)
 session.commit()
 
-item3 = CategoryItem(name="xylophone", user_id=1, category=category3)
+item3 = CategoryItem(name="xylophone", user_id=1, description="The xylophone is a musical instrument in the percussion family that consists of wooden bars struck by mallets.", category=category3)
 
 session.add(item3)
 session.commit()
