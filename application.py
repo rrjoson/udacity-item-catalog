109e1ace454202f92a8fc01e9f50873db0100534
rrjoson <rr_joson@yahoo.com>
2016-11-04 02:55:55 +0800

    feat: Implement google logout feature

--- application.py
+++ application.py
@@ -147,6 +147,11 @@ def logout():
 		fbdisconnect()
 		del login_session['facebook_id']
 
+	if login_session['provider'] == 'google':
+		gdisconnect()
+		del login_session['gplus_id']
+		del login_session['access_token']
+
 	del login_session['username']
 	del login_session['email']
 	del login_session['picture']
@@ -270,10 +275,10 @@ def gconnect():
 		response.headers['Content-Type'] = 'application/json'
 		return response
 
-	stored_credentials = login_session.get('credentials')
+	stored_access_token = login_session.get('access_token')
 	stored_gplus_id = login_session.get('gplus_id')
 
-	if stored_credentials is not None and gplus_id == stored_gplus_id:
+	if stored_access_token is not None and gplus_id == stored_gplus_id:
 		response = make_response(json.dumps('Current user is already connected.'), 200)
 		response.headers['Content-Type'] = 'application/json'
 		return response
@@ -302,9 +307,25 @@ def gconnect():
 
 	return "Login Successful"
 
-@app.route('/gdicconnect')
+@app.route('/gdisconnect')
 def gdisconnect():
-	return "This will log you out. (Google)"
+	# Only disconnect a connected user.
+	access_token = login_session.get('access_token')
+
+	if access_token is None:
+		response = make_response(json.dumps('Current user not connected.'), 401)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+
+	url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
+	h = httplib2.Http()
+	result = h.request(url, 'GET')[0]
+
+	if result['status'] != '200':
+	    # For whatever reason, the given token was invalid.
+	    response = make_response(json.dumps('Failed to revoke token for given user.'), 400)
+	    response.headers['Content-Type'] = 'application/json'
+	    return response
 
 @app.route('/catalog/JSON')
 def showCategoriesJSON():
